587fc3858591d4060355a162306d3861cd1a8c02
David Vodzinskyi <nikeron2002@gmail.com>
2022-06-02 14:54:45 +0300

    added page guaratees, some fixes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,37 @@
-$(function(){
-   
+
+const swiper = new Swiper('.popular-offers__slider', {
+   // loop: true,
+   pagination: {
+      el: '.popular-offers__pagination',
+   },
+   breakpoints: {
+      319: {
+         slidesPerView: 2,
+         slidesPerGroup: 2,
+      },
+      470: {
+         slidesPerView: 3,
+         slidesPerGroup: 3,
+      },
+      554: {
+         slidesPerView: 2,
+         slidesPerGroup: 2,
+      },
+      930: {
+         slidesPerView: 3,
+         slidesPerGroup: 3,
+      },
+      1220: {
+         slidesPerView: 4,
+         slidesPerGroup: 4,
+      },
+      1561: {
+         slidesPerView: 5,
+         slidesPerGroup: 5,
+         spaceBetween: 55,
+         slideToClickedSlide: true,
+      }
+
+   }
+
 });
