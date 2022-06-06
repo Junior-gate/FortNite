a4d86c890de7da1b94e7fa33c2b36474507f496e
SaneDoubleTea <doubletea2015@gmail.com>
2022-06-06 13:53:36 +0300

    fixed_header-on-scroll

--- js/header.js
+++ js/header.js
@@ -25,14 +25,11 @@ function toggleMobilenav() {
   body.classList.toggle("no-scroll");
 }
 
-
-
-
 //статичный хедер при скролле
 (function () {
   const header = document.querySelector(".header");
   window.onscroll = () => {
-    if (window.pageYOffset > 50) {
+    if (window.pageYOffset > 100) {
       header.classList.add("header--active");
     } else {
       header.classList.remove("header--active");
@@ -40,40 +37,3 @@ function toggleMobilenav() {
   };
 })();
 
-// Scroll to anchors
-(function () {
-  const smoothScroll = function (targetEl, duration) {
-    const headerElHeight = document.querySelector(".header").clientHeight;
-    let target = document.querySelector(targetEl);
-    let targetPosition = target.getBoundingClientRect().top - headerElHeight;
-    let startPosition = window.pageYOffset;
-    let startTime = null;
-
-    const ease = function (t, b, c, d) {
-      t /= d / 2;
-      if (t < 1) return (c / 2) * t * t + b;
-      t--;
-      return (-c / 2) * (t * (t - 2) - 1) + b;
-    };
-
-    const animation = function (currentTime) {
-      if (startTime === null) startTime = currentTime;
-      const timeElapsed = currentTime - startTime;
-      const run = ease(timeElapsed, startPosition, targetPosition, duration);
-      window.scrollTo(0, run);
-      if (timeElapsed < duration) requestAnimationFrame(animation);
-    };
-    requestAnimationFrame(animation);
-  };
-
-  const scrollTo = function () {
-    const links = document.querySelectorAll(".js-scroll");
-    links.forEach((each) => {
-      each.addEventListener("click", function () {
-        const currentTarget = this.getAttribute("href");
-        smoothScroll(currentTarget, 1000);
-      });
-    });
-  };
-  scrollTo();
-})();
