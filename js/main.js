d0ffe7e2bed677af419105346c1a89094a82479f
David Vodzinskyi <nikeron2002@gmail.com>
2022-06-09 12:39:15 +0300

    index fixe scroll catalog

--- js/main.js
+++ js/main.js
@@ -100,3 +100,59 @@ initSelect(navigationSelect);
 
 
 
+const showMore = document.querySelector('.show-more');
+const catalogBtn = document.querySelector('.catalog__btn');
+const catalogLess = document.querySelector('.catalog__btn-less');
+
+
+catalogBtn.addEventListener('click', function () {
+	showMore.classList.toggle('show-more--active');
+	catalogBtn.classList.toggle('catalog__btn--active');
+	catalogLess.classList.toggle('catalog__btn-less--active');
+});
+
+catalogLess.addEventListener('click', function () {
+	showMore.classList.toggle('show-more--active');
+	catalogBtn.classList.toggle('catalog__btn--active');
+	catalogLess.classList.toggle('catalog__btn-less--active');
+});
+
+
+// Scroll to catalog
+(function () {
+
+	const smoothScroll = function (targetEl, duration) {
+		const headerElHeight = document.querySelector('.catalog').clientHeight;
+		let target = document.querySelector(targetEl);
+		let targetPosition = target.getBoundingClientRect().top - headerElHeight;
+		let startPosition = window.pageYOffset;
+		let startTime = null;
+
+		const ease = function (t, b, c, d) {
+			t /= d / 2;
+			if (t < 1) return c / 2 * t * t + b;
+			t--;
+			return -c / 2 * (t * (t - 2) - 1) + b;
+		};
+
+		const animation = function (currentTime) {
+			if (startTime === null) startTime = currentTime;
+			const timeElapsed = currentTime - startTime;
+			const run = ease(timeElapsed, startPosition, targetPosition, duration);
+			window.scrollTo(0, run);
+			if (timeElapsed < duration) requestAnimationFrame(animation);
+		};
+		requestAnimationFrame(animation);
+	};
+
+	const scrollTo = function () {
+		const links = document.querySelectorAll('.pure-scroll');
+		links.forEach(each => {
+			each.addEventListener('click', function () {
+				const currentTarget = this.getAttribute('href');
+				smoothScroll(currentTarget, 1000);
+			});
+		});
+	};
+	scrollTo();
+}());
